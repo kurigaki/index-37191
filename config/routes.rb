3c7f13ae0fdaecd7c853d8cb4e9428de12b2d823
kurigaki <kenji.kurigaki@gmail.com>
2021-12-18 17:57:19 +0900

    Revert "マイページの実装"
    This reverts commit ac56be4dc078656528fd2473e9a7e9c487ef5a3b.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update]
   resources :posts do
     collection do
       get 'search'
